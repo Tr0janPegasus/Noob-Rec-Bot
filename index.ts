6f0dd635efd829008fe09b358fe7fb173adf87bf
Avi Avinav <aviavinav2004@gmail.com>
2022-01-14 12:48:16 +0530

    add new command

--- index.ts
+++ index.ts
@@ -12,15 +12,16 @@ client.on("ready", () => {
 });
 
 client.on("messageCreate", (message) => {
-  if (message.content.split(" ")[0] === "recommend") {
+  if (message.content.split(" ")[0] === ">recommend") {
     if (message.content.split(" ")[1] === "python") {
       message.reply({
         content:
-          "Check out the following links:\n 1) Official Docs - https://www.python.org/\n 2) Python Tutorial - https://www.youtube.com/watch?v=rfscVS0vtbw\n",
+          "Check out the following links:\n\n 1) Official Docs - https://www.python.org/\n 2) Python Tutorial - https://www.youtube.com/watch?v=rfscVS0vtbw\n",
       });
-    } else {
+    } else if (message.content.split(" ")[1] === "webdev") {
       message.reply({
-        content: "Avi is God!",
+        content:
+          "Check out the following links:\n\n 1) Web Dev Simplified Channel - https://www.youtube.com/c/WebDevSimplified\n 2) Sonny Sangha's Channel for React.js - https://www.youtube.com/c/SonnySangha\n 3) Fireship Channel - https://www.youtube.com/c/Fireship\n 4) Traversy Media Channel - The God of Web Dev Tutorials - https://www.youtube.com/c/TraversyMedia\n 5) Jack Herrigton's Channel for Typescript - https://www.youtube.com/c/JackHerrington\n 6) Kevin Powell - The CSS King - https://www.youtube.com/kepowob",
       });
     }
   }
