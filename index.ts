8b5866330673caf48f8f2f8b660db8164620823a
Avi Avinav <aviavinav2004@gmail.com>
2022-01-14 11:49:29 +0530

    Add Check Command

--- index.ts
+++ index.ts
@@ -7,7 +7,9 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.on("ready", () => {});
+client.on("ready", () => {
+  console.log("Bot is Ready!");
+});
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
